0d9bed0df77e58c6cd893f619aa095bb18de898c
dargonJ24 <dargonj24@gmail.com>
2023-06-28 21:41:06 +0700

    test nodemon

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ const express =require('express')
 const app=express()
 const port =3000;
 app.get('/',(req,res)=>{res.send('hello')})
-app.listen(port,console.log("App listening on port "+ port))
+app.listen(port,console.log("App listening on  port "+ port))
