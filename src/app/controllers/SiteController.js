b5bcfc76ad84ed30c9fed06ce16aa381684c065b
dargonJ24 <dargonj24@gmail.com>
2023-06-30 21:02:00 +0700

    fix bug show data for document

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,17 +1,11 @@
 const Course =require("../models/Course")
 class SiteController {
- index(req,res,next)
- 
- {
-  Course.find({})
-  .then(course=>{
-    course =course.map(course=>course.toObject)
-    res.render('home',{course})
-
-  })
-  
-  .catch(error=>next(error));
-
- }
+  index(req, res, next) {
+    Course
+      .find({})
+      .lean()
+      .then((Courses) => res.render("home", { Courses }))
+      .catch(next);
+  }
 }
   module.exports=new SiteController;
