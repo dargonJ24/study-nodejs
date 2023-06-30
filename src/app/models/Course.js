6a36de6b35fa0997b23a5984d3504e91eccb43cb
dargonJ24 <dargonj24@gmail.com>
2023-06-30 15:57:00 +0700

    create model mongoose

---
+++ src/app/models/Course.js
@@ -0,0 +1,7 @@
+const mongoose =require('mongoose')
+const Schema =mongoose.Schema;
+const Course =new Schema({name: {type:String ,maxLength:255},
+    description:{type:String, maxLength : 600},
+    image : {type:String,maxLength:255}
+})
+module.exports=mongoose.model('Course',Course)
