00b3d1bf41573c1cf8446b87341ede9f88c68e4d
dargonJ24 <dargonj24@gmail.com>
2023-07-01 23:00:26 +0700

    update link & code ui page home

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -2,9 +2,8 @@ const express =require('express')
 const router =express.Router();
 const courseController =require('../app/controllers/CourseController')
 //newcontrller.index
-
+router.use('/create',courseController.create)
 router.post('/store',courseController.store)
-router.use('/:create',courseController.create)
 router.use('/:slug',courseController.show)
 
 module.exports=router
