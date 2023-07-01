e18b3046a2d179f8183630727c49c0c6f5dc7507
dargonJ24 <dargonj24@gmail.com>
2023-07-01 16:13:39 +0700

    read document by filed

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -3,9 +3,21 @@ const Course =require("../models/Course")
 class CourseController{
    
     // GET /news/:slug
-    show(req,res)
-    {
-        res.send("COURSE DETAIL"+ req.params.slug)
-    }
+    show(req, res, next) {
+        Course.findOne({ slug: req.params.slug })
+          .then((course) => {
+            if (!course) {
+              // If the course is not found, send a 404 response
+              res.status(404).json({ error: 'Course not found' });
+            } else {
+              // If the course is found, send a JSON response
+              res.json(course);
+            }
+          })
+          .catch((err) => {
+            // Handle the error and send an appropriate response
+            next(err);
+          });
+      }
 }
 module.exports =new CourseController;
